e35b79b2d6aa4ee4fe16fe7aec3702f3dc76c348
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2023-03-26 16:35:10 +0200

    End screens

--- Assets/EndScreenController.cs
+++ Assets/EndScreenController.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndScreenController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private SceneReference _startScene;
+    
+    [Space]
+    
+    [SerializeField]
+    private Image _backgroundImage;
+    
+    [SerializeField]
+    private Color _fadeOutColor = Color.black;
+
+    [SerializeField]
+    private float _fadeOutDuration = 3f;
+
+    [SerializeField]
+    private Ease _fadeOutEase = Ease.OutCubic;
+
+    [SerializeField]
+    private float _sceneChangeDelay = 1f;
+
+    
+    
+    private void Update()
     {
-        
+        if (Input.anyKeyDown ||
+            Input.GetButtonDown("Rhi") ||
+            Input.GetButtonDown("No") ||
+            Input.GetButtonDown("Ce") ||
+            Input.GetButtonDown("Ros")) {
+            ReloadGame();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Tween FadeOut()
     {
-        
+        return _backgroundImage.DOColor(_fadeOutColor, _fadeOutDuration).SetEase(_fadeOutEase);
+    }
+
+    private void ReloadGame()
+    {
+        var seq = DOTween.Sequence();
+        seq.Append(FadeOut());
+        seq.AppendInterval(_sceneChangeDelay);
+        seq.AppendCallback(LoadScene);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(_startScene);
     }
 }
