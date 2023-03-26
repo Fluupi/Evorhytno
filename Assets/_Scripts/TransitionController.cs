8da5021c769af8c87dc9cea1d6d9a4c070528a39
Matthieu Golsenne <matthieu.golsenne@gmail.com>
2023-03-26 16:09:09 +0200

    ROS

--- Assets/_Scripts/TransitionController.cs
+++ Assets/_Scripts/TransitionController.cs
@@ -1,14 +1,27 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 public class TransitionController : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip _transitionClip;
+
+    [SerializeField]
+    private AudioSource _sfxSource;
+    
     private Animator _animator;
 
     private readonly int _trigger = Animator.StringToHash("Play");
 
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     public void PlayTransition()
     {
         _animator.SetTrigger(_trigger);
+        _sfxSource.PlayOneShot(_transitionClip);
     }
 }
